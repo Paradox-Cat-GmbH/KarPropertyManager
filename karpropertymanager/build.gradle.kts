714fd721684e574dddf7fbf7958ac4d87fdbddd1
Mohammad Tauqir Ansari <Mohammad.Ansari@partner.bmw.de>
2025-03-13 15:39:03 +0100

    Added publishing

--- karpropertymanager/build.gradle.kts
+++ karpropertymanager/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    `maven-publish`
+    signing
 }
 
 android {
@@ -41,4 +43,62 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+val libVersion = "1.0.0"
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.paradoxcat"
+            artifactId = "karpropertymanager"
+            version = libVersion
+
+            afterEvaluate {
+                from(components["release"])
+            }
+
+        }
+    }
+
+    repositories {
+        maven {
+            name = "maven"
+            url = uri(layout.buildDirectory.dir("maven"))
+        }
+    }
+}
+
+signing {
+    // Use installed GPG rather than built-in outdated version
+    useGpgCmd()
+// Sign all publications I guess
+    sign(publishing.publications)
+//sign(publishing.publications["release"])
+}
+
+tasks.register<Zip>("generateUploadPackage") {
+    // Take the output of our publishing
+    val publishTask = tasks.named(
+        "publishReleasePublicationToMavenRepository",
+        PublishToMavenRepository::class.java)
+
+    from(publishTask.map { it.repository.url })
+
+// Exclude maven-metadata.xml as Sonatype fails upload validation otherwise
+    exclude {
+        // Exclude left over directories not matching current version
+        // That was needed otherwise older versions empty directories would be include in our ZIP
+        if (it.file.isDirectory && it.path.matches(Regex(""".*\d+\.\d+.\d+$""")) && !it.path.contains(libVersion)) {
+            return@exclude true
+        }
+
+        // Only take files inside current version directory
+        // Notably excludes maven-metadata.xml which Maven Central upload validation does not like
+        (it.file.isFile && !it.path.contains(libVersion))
+    }
+
+    into("karpropertymanager")
+    // Name of zip file
+    archiveFileName.set("karpropertymanager.zip")
 }
